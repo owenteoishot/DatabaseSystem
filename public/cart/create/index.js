a564eb6ced80474dd1db89a553c4d6e8af4369b0
owenteoishot <OWENTEO20.24@ichat.sp.edu.sg>
2025-05-26 15:39:46 +0800

    cleared the frontend template (havent populate)

--- public/cart/create/index.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener('DOMContentLoaded', function () {
-    const token = localStorage.getItem("token");
-    const cartProductId = localStorage.getItem("cartProductId");
-
-    const productIdInput = document.querySelector("input[name='productId']");
-    productIdInput.value = cartProductId;    
-
-
-});
